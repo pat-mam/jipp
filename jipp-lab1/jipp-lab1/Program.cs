59d822db14c2a21f52615ba985536aa2158cc1b1
Pat Ma <mamontowicz@o2.pl>
2025-02-23 20:14:19 +0100

    zadanie 1

--- jipp-lab1/jipp-lab1/Program.cs
+++ jipp-lab1/jipp-lab1/Program.cs
@@ -6,7 +6,63 @@ namespace jipp_lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // //Console.WriteLine("Podaj swoje imię:");
+           // //var name = Console.ReadLine();
+           // //Console.WriteLine("Hello " + name);
+
+           //// int result = 5 + 9;
+
+
+
+           // int number=5;
+           // double money=1.2;
+           // string text="jakis tekst";
+           // bool isLogged=true;
+           // char myChar='y';
+           // decimal price=2.22m;
+           // Console.WriteLine($"number = {number},\nmoney = {money},\ntext = {text},\nisLogged = {isLogged},\nmyChar = {myChar}\nprice = {price} ");
+
+
+           // string myAge = "Age: ";
+           // int wifeAge = 18;
+           // string result = myAge + wifeAge;
+           // Console.WriteLine(result);
+           // operacja zamienia zmienna wifeAge na string, nastepnie dodaje tekst do jednej zmiennej.
+           // bool isTrue = true;
+           // bool isFalse = false;
+           // bool isReallyTrue = true;
+
+           // bool and = isTrue && isFalse;
+           // bool or = isTrue || isFalse;
+           // bool negative = !isFalse;
+
+           // Console.Write($"and = {and},\nor = {or},\nnegative = {negative}\n");
+
+           // //Utwórz dwie zmienne o nazwach a, b i przypisz do nich wartości 5, 12.
+           // //Utwórz zmienne o nazwach add, sub, div, mul, mod i przypisz do nich kolejno wynik dodawania, odejmowania, dzielenia, mnożenia i modulo zmiennych a i b.
+           // // Wyświetl zmienne add, sub, div, mul, mod w konsoli.
+
+           // int a = 5, b = 12;
+           // int add = a + b;
+           // int sub = a - b;
+           // int div = a / b;
+           // int mul = a * b;
+           // int mod = a % b;
+           // Console.WriteLine($"a = {a}, b = {b}, a + b = {add}, a - b = {sub}, a / b = {div}, a * b = {mul}, a % b = {mod}");
+
+            string a = "Ala";
+            string b = "ma";
+            string c = "kota.";
+
+            string result = a + b + c;
+            Console.WriteLine(result);
+            // operacja dodaje zmienne string bez pustych znakow.
+
+
+
+
+
+
         }
     }
 }
